e89f09b0bedc5cdd4160ecd91b0346b697aa2af3
timmcmic <timmcmic@microsoft.com>
2024-10-07 20:36:47 -0400

    Updating

--- Office365IPAddress.ps1
+++ Office365IPAddress.ps1
@@ -316,6 +316,73 @@ function test-IPSpace
     out-logfile -string "Exiting test-IPSpace"
 }
 
+function test-URLSpace
+{
+    Param
+    (
+        [Parameter(Mandatory = $true)]
+        $dataToTest,
+        [Parameter(Mandatory = $true)]
+        $IPAddress,
+        [Parameter(Mandatory = $true)]
+        $RegionString
+    )
+
+    $functionNetwork = $NULL
+
+    out-logfile -string "Entering test-IPSpace"
+
+    foreach ($entry in $dataToTest)
+    {
+        Out-logfile -string ("Testing entry id: "+$entry.id)
+
+        if ($entry.ips.count -gt 0)
+        {
+            out-logfile -string "IP count > 0"
+
+            foreach ($ipEntry in $entry.ips)
+            {
+                out-logfile -string ("Testing entry IP: "+$ipEntry)
+
+                 $functionNetwork = [System.Net.IpNetwork]::Parse($ipEntry)
+
+                 out-logfile -string ("BaseAddress: "+$functionNetwork.baseAddress+ " PrefixLength: "+$functionNetwork.PrefixLength)
+
+                 if ($functionNetwork.Contains($IPAddress))
+                 {
+                    out-logfile -string "The IP to test is contained within the entry.  Log the service."
+
+                    $outputObject = new-Object psObject -property @{
+                        M365Instance = $regionString
+                        ID = $entry.ID
+                        ServiceAreaDisplayName = $entry.ServiceAreaDisplayName
+                        URLs = $entry.URLs
+                        IPs = $entry.ips
+                        IPInSubnet = $ipEntry
+                        TCPPorts = $entry.tcpports
+                        ExpressRoute = $entry.expressRoute
+                        Required = $entry.required
+                    }
+
+                    out-logfile -string $outputObject
+
+                    $global:outputArray += $outputObject
+                 }
+                 else
+                 {
+                    out-logfile -string "The IP to test is not contained within the entry - move on."
+                 }
+            }
+        }
+        else 
+        {
+            out-logfile -string "IP count = 0 -> skipping"
+        }
+    }
+
+    out-logfile -string "Exiting test-IPSpace"
+}
+
 function test-IPChangeSpace
 {
     Param
@@ -506,6 +573,36 @@ function get-IPLocationInformation
     return $functionData
 }
 
+function test-Parameters
+{
+    Param
+    (
+        [Parameter(Mandatory = $true)]
+        $ipAddresToTest,
+        [Parameter(Mandatory = $true)]
+        $URLToTest
+    )
+
+    if (($URLToTest -eq "") -and ($IPAddressToTest -eq ""))
+    {
+        out-logfile -string "No URL or IP entries were specified.  To perform an analysis specify either an IP address or URL."
+        out-logfile -string "ERROR: NO IP OR URL SPECIFIED" -isERROR:$TRUE
+    }
+    elseif (($URLToTest -ne "") -and ($IPAddressToTest -ne ""))
+    {
+        out-logfile -string "Both a URL and IP address were specified to test.  Specify only a IP Address or URL to proceed - not both."
+        out-logfile -string "ERROR: URL AND IP ADDRESS SPECIFIED" -isERROR:$TRUE
+    }
+    elseif ($URLToTest -ne "")
+    {
+        out-logfile -string "URL specified to test - proceed."
+    }
+    elseif ($IPAddressToTest -ne "")
+    {
+        out-logfile -string "IP specified to test - proceed."
+    }
+}
+
 Function Test-PowershellVersion
      {
         [cmdletbinding()]
@@ -621,6 +718,8 @@ out-logfile -string "Start Office365IPAddress"
 out-logfile -string "*********************************************************************************"
 
 Test-PowerShellVersion
+
+Test-Parameters -ipAddressToTest $ipAddressToTest -urlToTest $urlTest
 
 out-logfile -string "Obtaining client guid for web requests."
 

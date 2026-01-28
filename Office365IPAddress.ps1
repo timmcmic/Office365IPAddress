6466d8b98bdbe8821d03946189a433c7b9a1a03c
Timothy J. McMichael <timmcmic@microsoft.com>
2026-01-28 10:18:48 -0500

    Updating

--- Office365IPAddress.ps1
+++ Office365IPAddress.ps1
@@ -1804,7 +1804,7 @@ function test-AzureScript
         $logFolderPath
     )
 
-    $requiredScriptVersion = "1.7"
+    $requiredScriptVersion = "1.8"
 
     try {
         $version = Invoke-Command -ScriptBlock {AzureIPAddress.ps1 -logFolderPath $args[0] -versionTest $args[1] -errorAction Stop} -ArgumentList $logFolderPath,$true
